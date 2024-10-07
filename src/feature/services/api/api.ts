fe68c16cc9d08d626f7ad83ec3a0ed3939ca2ba0
IsaiasMella <67384494+IsaiasMella@users.noreply.github.com>
2024-10-07 04:20:28 -0300

    Update api.ts

--- src/feature/services/api/api.ts
+++ src/feature/services/api/api.ts
@@ -8,7 +8,7 @@ export const api: AxiosInstance = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  const token = "";
+  const token = "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";
   config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
